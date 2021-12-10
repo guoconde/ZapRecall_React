092bceb864e137965a1dbf3d61d24b5844178fa0
Gustavo Conde <guoconde@pop-os.localdomain>
2021-12-10 13:20:41 -0300

    Implementando logica das respostas

--- src/Componentes/Cartoes.js
+++ src/Componentes/Cartoes.js
@@ -15,6 +15,13 @@ function Cartao() {
     
     const [ quantidadeCartoes, setQuantidadeCartoes ] = useState([1])
     let [ ladoDoCartao, setLadoDoCartao ] = useState('frente')
+    let [ respostaDoCartao, setRespostaDoCartao ] = useState('')
+
+    if(respostaDoCartao !== '' && respostaDoCartao === 'resposta-certa') {
+        console.log('esta certo')
+    } else {
+        console.log('esta errado')
+    }
 
     function virarCartao() {
         if(ladoDoCartao === 'frente') {
@@ -26,35 +33,53 @@ function Cartao() {
 
     return (
             <>
-                {ladoDoCartao === 'frente' ? <CartaoPergunta quantidadeCartoes={quantidadeCartoes} /> : <CartaoResposta />}
+                {ladoDoCartao === 'frente' ? <CartaoPergunta /> : <CartaoResposta />}
             </>        
     )
+    
+    function CartaoPergunta() {
+        return (
+            <div className={`cartoes frente`}
+             onClick={virarCartao}>
+                <div className='topo-cartao'>
+                    <div>{ quantidadeCartoes }/{ quantidadeCartoes.length }</div>
+                </div>
+                <div className='pergunta'>O que é JSX?</div>
+                <BotaoVoltar />
+            </div>
+                )
+            }
+            
+    function CartaoResposta() {
+
+        return (
+            <div className={`cartoes verso ${respostaDoCartao}`}>
+                <div className='topo-cartao'>
+                    <span>O que é JSX?</span>
+                    <div>{ quantidadeCartoes }/{ quantidadeCartoes.length }</div>
+                </div>
+                <div className='resposta'>É uma extensão da linguagem do JavaScript.</div>
+                {respostaDoCartao !== '' ? <BotaoVoltar /> : <Respostas />}
+            </div>
+        )
+    }
+
+    function Respostas() {
+        return (
+            <div className='respostas'>
+                <button className='btn resposta-aprendida' onClick={() => setRespostaDoCartao('resposta-aprendida')}>Aprendi agora</button>
+                <button className='btn resposta-ausente' onClick={() => setRespostaDoCartao('resposta-ausente')}>Não lembrei</button>
+                <button className='btn resposta-demorada' onClick={() => setRespostaDoCartao('resposta-demorada')}>Lembrei com esforço</button>
+                <button className='btn resposta-certa' onClick={() => setRespostaDoCartao('resposta-certa')}>Zap!</button>
+            </div>
+        )
+    }
+
+    function BotaoVoltar() {
+        return (
+            <img src={ btnVoltar } alt="Botao Voltar" />
+        )
+    }
 }
 
-function CartaoPergunta(props) {
-    return (
-        <div className={`cartoes frente`}
-         onClick={props.virarCartao}>
-            <div className='topo-cartao'>
-                <div>{ props.quantidadeCartoes }/{ props.quantidadeCartoes.length }</div>
-            </div>
-            <div className='pergunta'>O que é JSX?</div>
-            <img src={ btnVoltar } alt="Botao Voltar" />
-        </div>
-            )
-        }
-        
-function CartaoResposta(props) {
-    return (
-        <div className={`cartoes verso`}
-         onClick={props.virarCartao}>
-            <div className='topo-cartao'>
-                <span>O que é JSX?</span>
-                <div>{ props.quantidadeCartoes }/{ props.quantidadeCartoes.length }</div>
-            </div>
-            <div className='resposta'>É uma extensão da linguagem do JavaScript.</div>
-            <img src={ btnVoltar } alt="Botao Voltar" />
-        </div>
-    )
-}
 
